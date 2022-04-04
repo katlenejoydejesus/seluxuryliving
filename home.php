620d7cad4199ade73b652e08f8b978d4bb83840f
katlene joy <katlenejoy.dejesus@gmail.com>
2022-04-04 09:43:10 +0800

    First commit

--- home.php
+++ home.php
@@ -1,17 +1,140 @@
 <?php get_header(); ?>
-<!-- slideshow -->
-<section class="hp-slideshow">
-    <div class="region-slideshow-container">
-        <h2 class="hidden">hidden h2</h2>
-        <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar("Hp Slideshow") ) : ?>
-        <?php endif ?>
-    </div>
-</section>
+<!-- slideshow --> <section class="hp-slideshow"> <div class="region-slideshow-container">
+<h2 class="hidden">Slideshow</h2> <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar("Hp Slideshow") ) : ?> <?php endif ?>
+</div> </section>
 <!-- slideshow end -->
-<!-- welcome -->
-<section class="hp-welcome">
-    <div class="region-welcome-container">
-    </div>
-</section>
+<!-- properties --> <section class="hp-properties"> <div class="region-properties-container">
+<div class="properties-gallery-image-container">
+<a href="[blogurl]" aria-label="properties-image" class="properties-image"> <div class="properties-item canvas-wrapper"> <canvas width="370" height="370"></canvas> <img alt="properties-image" class="canvas-img img-responsive" src=" [stylesheet_directory]/images/properties-1.jpg">
+<span class="ai-font-instagram"></span>
+</div> </a>
+<a href="[blogurl]" aria-label="properties-image" class="properties-image"> <div class="properties-item canvas-wrapper"> <canvas width="370" height="370"></canvas> <img alt="properties-image" class="canvas-img img-responsive" src=" [stylesheet_directory]/images/properties-2.jpg">
+<span class="ai-font-instagram"></span>
+</div> </a>
+<a href="[blogurl]" aria-label="properties-image" class="properties-image"> <div class="properties-item canvas-wrapper"> <canvas width="370" height="370"></canvas> <img alt="properties-image" class="canvas-img img-responsive" src=" [stylesheet_directory]/images/properties-3.jpg">
+<span class="ai-font-instagram"></span>
+</div> </a>
+<a href="[blogurl]" aria-label="properties-image" class="properties-image"> <div class="properties-item canvas-wrapper"> <canvas width="370" height="370"></canvas> <img alt="properties-image" class="canvas-img img-responsive" src=" [stylesheet_directory]/images/properties-4.jpg">
+<span class="ai-font-instagram"></span>
+</div> </a>
+<a href="[blogurl]" aria-label="properties-image" class="properties-image"> <div class="properties-item canvas-wrapper"> <canvas width="370" height="370"></canvas> <img alt="properties-image" class="canvas-img img-responsive" src=" [stylesheet_directory]/images/properties-5.jpg">
+<span class="ai-font-instagram"></span>
+</div> </a>
+<a href="[blogurl]" aria-label="properties-image" class="properties-image"> <div class="properties-item canvas-wrapper"> <canvas width="370" height="370"></canvas> <img alt="properties-image" class="canvas-img img-responsive" src=" [stylesheet_directory]/images/properties-6.jpg">
+<span class="ai-font-instagram"></span>
+</div> </a>
+</div>
+</div> </section>
+<!-- properties end -->
+<!-- quick-search --> <section class="hp-quick-search"> <div class="region-quick-search-container">
+<div class="quick-search-form">
+<div class="primary-qs-item site-title" >
+<h3>Quick</h3> <h2>Search</h2>
+</div>
+<div class="secondary-qs-item" >
+<select aria-label="qs-fields"> <option value="SFR,CND">House / Condo</option> <option value="SFR">House Only</option> <option value="CND">Condo Only</option> <option value="RI">Multi-Family</option> <option value="COM">Commercial</option> <option value="LL">Lots / Land</option> <option value="RI">Multi-Unit Residential</option> <option value="MH">Mobile Home</option> <option value="RNT">Rental</option> <option value="FRM">Farms</option> </select>
+</div>
+<div class="tertiary-qs-item" >
+<input type="text" placeholder="City or Zip" aria-label="qs-fields" />
+</div>
+<div class="fourth-qs-item" >
+<select aria-label="qs-fields"> <option value="">Beds</option> <option value="1">1+</option> <option value="2">2+</option> <option value="3">3+</option> <option value="4">4+</option> <option value="5">5+</option> </select>
+</div>
+<div class="fifth-qs-item" >
+<select aria-label="qs-fields"> <option value="">Baths</option> <option value="1">1+</option> <option value="2">2+</option> <option value="3">3+</option> <option value="4">4+</option> <option value="5">5+</option> </select>
+</div>
+<div class="sixth-qs-item" >
+<input type="text" placeholder="Min. Price" aria-label="qs-fields" />
+</div>
+<div class="seventh-qs-item" >
+<input type="text" placeholder="Max. Price" aria-label="qs-fields" />
+</div>
+<div class="eighth-qs-item" >
+<div class="qs-search"> <input type="submit" value="Search" aria-label="qs-submit" /> </div>
+</div>
+<div class="ninth-qs-item" >
+<div class="qs-advanced"> <a href="[blogurl]" aria-label="qs-btn"> <span>Advanced</span> </a> </div>
+</div>
+</div>
+</div> </section>
+<!-- quick-search end -->
+<!-- welcome --> <section class="hp-welcome"> <div class="region-welcome-container">
+<div class="welcome-gallery-image-container">
+<a href="[blogurl]" aria-label="welcome-image" class="welcome-image"> <div class="welcome-item canvas-wrapper"> <canvas width="370" height="370"></canvas> <img alt="welcome-image" class="canvas-img img-responsive" src=" [stylesheet_directory]/images/welcome-1.jpg">
+<span class="ai-font-instagram"></span>
+</div> </a>
+<a href="[blogurl]" aria-label="welcome-image" class="welcome-image"> <div class="welcome-item canvas-wrapper"> <canvas width="370" height="370"></canvas> <img alt="welcome-image" class="canvas-img img-responsive" src=" [stylesheet_directory]/images/welcome-2.jpg">
+<span class="ai-font-instagram"></span>
+</div> </a>
+<a href="[blogurl]" aria-label="welcome-image" class="welcome-image"> <div class="welcome-item canvas-wrapper"> <canvas width="370" height="370"></canvas> <img alt="welcome-image" class="canvas-img img-responsive" src=" [stylesheet_directory]/images/welcome-3.jpg">
+<span class="ai-font-instagram"></span>
+</div> </a>
+<a href="[blogurl]" aria-label="welcome-image" class="welcome-image"> <div class="welcome-item canvas-wrapper"> <canvas width="370" height="370"></canvas> <img alt="welcome-image" class="canvas-img img-responsive" src=" [stylesheet_directory]/images/welcome-4.jpg">
+<span class="ai-font-instagram"></span>
+</div> </a>
+<a href="[blogurl]" aria-label="welcome-image" class="welcome-image"> <div class="welcome-item canvas-wrapper"> <canvas width="370" height="370"></canvas> <img alt="welcome-image" class="canvas-img img-responsive" src=" [stylesheet_directory]/images/welcome-5.jpg">
+<span class="ai-font-instagram"></span>
+</div> </a>
+<a href="[blogurl]" aria-label="welcome-image" class="welcome-image"> <div class="welcome-item canvas-wrapper"> <canvas width="370" height="370"></canvas> <img alt="welcome-image" class="canvas-img img-responsive" src=" [stylesheet_directory]/images/welcome-6.jpg">
+<span class="ai-font-instagram"></span>
+</div> </a>
+</div>
+</div> </section>
 <!-- welcome end -->
+<!-- team --> <section class="hp-team"> <div class="region-team-container">
+</div> </section>
+<!-- team end -->
+<!-- our-team --> <section class="hp-our-team"> <div class="region-our-team-container">
+</div> </section>
+<!-- our-team end -->
+<!-- cta --> <section class="hp-cta"> <div class="region-cta-container">
+<div class="cta-gallery-image-container">
+<a href="[blogurl]" aria-label="cta-image" class="cta-image"> <div class="cta-item canvas-wrapper"> <canvas width="370" height="370"></canvas> <img alt="cta-image" class="canvas-img img-responsive" src=" [stylesheet_directory]/images/cta-1.jpg">
+<span class="ai-font-instagram"></span>
+</div> </a>
+<a href="[blogurl]" aria-label="cta-image" class="cta-image"> <div class="cta-item canvas-wrapper"> <canvas width="370" height="370"></canvas> <img alt="cta-image" class="canvas-img img-responsive" src=" [stylesheet_directory]/images/cta-2.jpg">
+<span class="ai-font-instagram"></span>
+</div> </a>
+<a href="[blogurl]" aria-label="cta-image" class="cta-image"> <div class="cta-item canvas-wrapper"> <canvas width="370" height="370"></canvas> <img alt="cta-image" class="canvas-img img-responsive" src=" [stylesheet_directory]/images/cta-3.jpg">
+<span class="ai-font-instagram"></span>
+</div> </a>
+<a href="[blogurl]" aria-label="cta-image" class="cta-image"> <div class="cta-item canvas-wrapper"> <canvas width="370" height="370"></canvas> <img alt="cta-image" class="canvas-img img-responsive" src=" [stylesheet_directory]/images/cta-4.jpg">
+<span class="ai-font-instagram"></span>
+</div> </a>
+<a href="[blogurl]" aria-label="cta-image" class="cta-image"> <div class="cta-item canvas-wrapper"> <canvas width="370" height="370"></canvas> <img alt="cta-image" class="canvas-img img-responsive" src=" [stylesheet_directory]/images/cta-5.jpg">
+<span class="ai-font-instagram"></span>
+</div> </a>
+<a href="[blogurl]" aria-label="cta-image" class="cta-image"> <div class="cta-item canvas-wrapper"> <canvas width="370" height="370"></canvas> <img alt="cta-image" class="canvas-img img-responsive" src=" [stylesheet_directory]/images/cta-6.jpg">
+<span class="ai-font-instagram"></span>
+</div> </a>
+</div>
+</div> </section>
+<!-- cta end -->
+<!-- social --> <section class="hp-social"> <div class="region-social-container">
+<div class="social-gallery-image-container">
+<a href="[blogurl]" aria-label="social-image" class="social-image"> <div class="social-item canvas-wrapper"> <canvas width="370" height="370"></canvas> <img alt="social-image" class="canvas-img img-responsive" src=" [stylesheet_directory]/images/social-1.jpg">
+<span class="ai-font-instagram"></span>
+</div> </a>
+<a href="[blogurl]" aria-label="social-image" class="social-image"> <div class="social-item canvas-wrapper"> <canvas width="370" height="370"></canvas> <img alt="social-image" class="canvas-img img-responsive" src=" [stylesheet_directory]/images/social-2.jpg">
+<span class="ai-font-instagram"></span>
+</div> </a>
+<a href="[blogurl]" aria-label="social-image" class="social-image"> <div class="social-item canvas-wrapper"> <canvas width="370" height="370"></canvas> <img alt="social-image" class="canvas-img img-responsive" src=" [stylesheet_directory]/images/social-3.jpg">
+<span class="ai-font-instagram"></span>
+</div> </a>
+<a href="[blogurl]" aria-label="social-image" class="social-image"> <div class="social-item canvas-wrapper"> <canvas width="370" height="370"></canvas> <img alt="social-image" class="canvas-img img-responsive" src=" [stylesheet_directory]/images/social-4.jpg">
+<span class="ai-font-instagram"></span>
+</div> </a>
+<a href="[blogurl]" aria-label="social-image" class="social-image"> <div class="social-item canvas-wrapper"> <canvas width="370" height="370"></canvas> <img alt="social-image" class="canvas-img img-responsive" src=" [stylesheet_directory]/images/social-5.jpg">
+<span class="ai-font-instagram"></span>
+</div> </a>
+<a href="[blogurl]" aria-label="social-image" class="social-image"> <div class="social-item canvas-wrapper"> <canvas width="370" height="370"></canvas> <img alt="social-image" class="canvas-img img-responsive" src=" [stylesheet_directory]/images/social-6.jpg">
+<span class="ai-font-instagram"></span>
+</div> </a>
+</div>
+</div> </section>
+<!-- social end -->
+<!-- contact --> <section class="hp-contact"> <div class="region-contact-container">
+<p>Sample</p> <div class="contact-form "> '[contact-form-7 html_class="use-floating-validation-tip" id="34" title="hp template form 2"] </div>
+</div> </section>
+<!-- contact end -->
 <?php get_footer(); ?>
